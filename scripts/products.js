0e6fc29d49eecf18e30e12496cbb2ce5d1eaa4eb
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-03-26 12:26:27 +0600

    product cards height issue fixed

--- scripts/products.js
+++ scripts/products.js
@@ -1,15 +1,15 @@
 const categorButtonsContainer = elementGive("category-btns");
 const productsContainer = elementGive("all-products-container");
 const tabButtonsContainer = elementGive("tab-buttons");
-const cartStore=elementGive('cart-store-container')
-const productsArea=elementGive('products-area')
-const cartArea=elementGive('carts-area')
-const modalParent=elementGive('card_modal')
-const modalInner=elementGive('details-show')
-const userCartTotal=elementGive('cart-total')
-const navCartCount=elementGive('cart-count-nav')
+const cartStore = elementGive("cart-store-container");
+const productsArea = elementGive("products-area");
+const cartArea = elementGive("carts-area");
+const modalParent = elementGive("card_modal");
+const modalInner = elementGive("details-show");
+const userCartTotal = elementGive("cart-total");
+const navCartCount = elementGive("cart-count-nav");
 
-let localStorageCartData=JSON.parse(localStorage.getItem('cartStore')) || []
+let localStorageCartData = JSON.parse(localStorage.getItem("cartStore")) || [];
 async function getCategoryList() {
   const getCateList = await fetch(
     "https://dummyjson.com/products/category-list",
@@ -58,7 +58,7 @@ function renderProductsDataUI(x) {
 
     cardDiv.innerHTML = `
 
-      <div class="product-card card bg-base-100 h-full w-full shadow-sm">
+      <div class="product-card card bg-base-100  w-full shadow-sm">
   <figure>
     <img
       src="${image}"
@@ -131,127 +131,108 @@ tabButtonsContainer.addEventListener("click", (e) => {
 
     tabButton.classList.add("btn-info");
 
-const innerTextBtn=tabButton.innerText
+    const innerTextBtn = tabButton.innerText;
 
-if(innerTextBtn==='Products'){
- 
-cartArea.classList.add('hidden')
-productsArea.classList.remove('hidden')
-  
-
-}
-else{
-  productsArea.classList.add('hidden')
-  cartArea.classList.remove('hidden')
-}
-
+    if (innerTextBtn === "Products") {
+      cartArea.classList.add("hidden");
+      productsArea.classList.remove("hidden");
+    } else {
+      productsArea.classList.add("hidden");
+      cartArea.classList.remove("hidden");
+    }
   }
 });
 
+async function specificProduct(id, cartAdd) {
+  const product = await fetch(`https://dummyjson.com/products/${id}`);
+  const convertProduct = await product.json();
 
-async function specificProduct(id,cartAdd){
-
-
-const product=await fetch(`https://dummyjson.com/products/${id}`)
-const convertProduct=await product.json()
-
-
-if(cartAdd){
-
-addToCart(convertProduct)
-
-}
-else{
-modalCall(convertProduct)
-
+  if (cartAdd) {
+    addToCart(convertProduct);
+  } else {
+    modalCall(convertProduct);
+  }
 }
 
+function addToCart(data) {
+  const { id, title, price } = data;
+
+  const cartObj = {
+    id,
+    title,
+    quantity: 1,
+    price,
+  };
+
+  const findCart = localStorageCartData.find((x) => x.id === id);
+
+  if (findCart) {
+    findCart.quantity += 1;
+    localStorage.setItem("cartStore", JSON.stringify(localStorageCartData));
+    renderCart(localStorageCartData);
+  } else {
+    localStorageCartData.push(cartObj);
+    localStorage.setItem("cartStore", JSON.stringify(localStorageCartData));
+    renderCart(localStorageCartData);
+  }
 }
 
-
-function addToCart(data){
-const {id,title,price}=data
-
-
-const cartObj={
-  id,title,
-quantity:1,
-  price,
-  
-}
-
-
-const findCart=localStorageCartData.find(x=>x.id===id)
-
-
-if(findCart){
-  findCart.quantity+=1
-localStorage.setItem('cartStore',JSON.stringify(localStorageCartData))
-  renderCart(localStorageCartData)
-}
-else{
-  localStorageCartData.push(cartObj)
-  localStorage.setItem('cartStore',JSON.stringify(localStorageCartData))
-  renderCart(localStorageCartData)
-}
-
-}
-
-function renderCart(x){
-
-  if(!x.length){
-
-    cartStore.innerHTML=''
-    const div=document.createElement('div')
-   div.innerHTML=`
+function renderCart(x) {
+  if (!x.length) {
+    cartStore.innerHTML = "";
+    const div = document.createElement("div");
+    div.innerHTML = `
     
     <h2 class=' text-7xl'><i class="text-blue-600 fa-solid fa-face-rolling-eyes"></i></h2>
     <p class='font-semibold text-2xl'>Your cart is Empthy</p>
-    `
-userCartTotal.innerText=0
-cartStore.appendChild(div)
-div.className='col-span-full text-center space-y-5'
-navCartCount.innerText=`+0`
-    return
+    `;
+    userCartTotal.innerText = 0;
+    cartStore.appendChild(div);
+    div.className = "col-span-full text-center space-y-5";
+    navCartCount.innerText = `+0`;
+    return;
   }
 
-  cartStore.innerHTML=''
+  cartStore.innerHTML = "";
 
-  
-  x.forEach((cartData)=>{
-
-    const {id,title,price,quantity}=cartData
-  const cart=document.createElement('div')
-  cart.innerHTML=`
+  x.forEach((cartData) => {
+    const { id, title, price, quantity } = cartData;
+    const cart = document.createElement("div");
+    cart.innerHTML = `
   
   <div class="card  bg-base-100 shadow-sm p-5"> 
 <h2 class="font-bold text-2xl">${title}</h2>
     <p class="font-medium flex justify-between text-lg">Quantity: <span>X<span>${quantity}</span></span></p>
-    <p class="font-medium flex justify-between text-lg">price: <span>$<span>${price*quantity}</span></span></p>
+    <p class="font-medium flex justify-between text-lg">price: <span>$<span>${price * quantity}</span></span></p>
     <div class="flex justify-end py-2"><button data-id='${id}' class="remove-btn btn btn-info">Remove</button></div>
 </div> 
 
-  `
-  cartStore.appendChild(cart)
-
-  })
+  `;
+    cartStore.appendChild(cart);
+  });
 
   // const getCartDataFromStorage=JSON.parse(localStorage.getItem('cartStore'))
-const calcTotal=localStorageCartData.reduce((acc,curr)=>acc+curr.price*curr.quantity,0)
-  userCartTotal.innerText=parseFloat(calcTotal.toFixed(2))
-navCartCount.innerText=`+${localStorageCartData.length}`
+  const calcTotal = localStorageCartData.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0,
+  );
+  userCartTotal.innerText = parseFloat(calcTotal.toFixed(2));
+  navCartCount.innerText = `+${localStorageCartData.length}`;
 }
 
+renderCart(localStorageCartData);
 
-renderCart(localStorageCartData)
+function modalCall(data) {
+  const {
+    title,
+    description,
+    price,
+    category,
+    brand,
+    images: [image],
+  } = data;
 
-function modalCall(data){
-
-const {title,description,price,category,brand,images:[image]}=data
-
-
-
-modalInner.innerHTML=`
+  modalInner.innerHTML = `
 <div><img src="${image}" alt="" class=" w-full rounded-lg"></div>
 
 
@@ -272,12 +253,9 @@ modalInner.innerHTML=`
 
 </div>
 
-`
+`;
 
-
-
-modalParent.showModal()
-
+  modalParent.showModal();
 }
 
 /**
@@ -343,53 +321,34 @@ modalParent.showModal()
 }
  */
 
-productsContainer.addEventListener('click',(e)=>{
+productsContainer.addEventListener("click", (e) => {
+  const targetElement = e.target;
 
-  const targetElement=e.target
+  if (targetElement.classList.contains("card-details")) {
+    const getId = targetElement.dataset.id;
 
-if(targetElement.classList.contains('card-details')){
+    specificProduct(Number(getId));
+  } else if (targetElement.classList.contains("cart-add-btn")) {
+    const getId = targetElement.dataset.id;
 
-  const getId=targetElement.dataset.id
-
- specificProduct(Number(getId))
-
-
-
-
-}
-
-else if(targetElement.classList.contains('cart-add-btn')){
-
-  const getId=targetElement.dataset.id
-
-
-specificProduct(Number(getId),true)
-
-}
-
-
-
-})
-
+    specificProduct(Number(getId), true);
+  }
+});
 
 // cart store event eventListener
 
-cartStore.addEventListener('click',(e)=>{
+cartStore.addEventListener("click", (e) => {
+  const removeBtn = e.target;
 
+  if (removeBtn.classList.contains("remove-btn")) {
+    const id = removeBtn.dataset.id;
 
-const removeBtn=e.target
+    localStorageCartData = localStorageCartData.filter(
+      (cart) => cart.id !== Number(id),
+    );
 
-if(removeBtn.classList.contains("remove-btn")){
+    localStorage.setItem("cartStore", JSON.stringify(localStorageCartData));
 
-  const id=removeBtn.dataset.id
-
-localStorageCartData=localStorageCartData.filter(cart=>cart.id!==Number(id))
-
-localStorage.setItem('cartStore',JSON.stringify(localStorageCartData))
-
-renderCart(localStorageCartData)
-
-}
-
-
-})
+    renderCart(localStorageCartData);
+  }
+});
